9eb0cea8a68da17ad0293eda0dc67de8c474b9bd
wdeil <wdeilim@gmail.com>
2018-10-15 16:08:03 +0800

    change the way how to install oh-my-zsh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -8,10 +8,21 @@ sudo apt install zsh git vim -y
 if [ -e ~/.oh-my-zsh ] ;  then
   [ -e ~/.oh-my-zsh/tools/upgrade.sh ] && sh ~/.oh-my-zsh/tools/upgrade.sh
 else
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  # sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  if [ ! -n "$ZSH" ]; then
+    ZSH=~/.oh-my-zsh
+  fi
+  umask g-w,o-w
+  git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git "$ZSH"
+  if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
+    mv ~/.zshrc ~/.zshrc.pre-oh-my-zsh;
+  fi
+  cp "$ZSH"/templates/zshrc.zsh-template ~/.zshrc
+  sed "/^export ZSH=/ c\\
+  export ZSH=\"$ZSH\"
+  " ~/.zshrc > ~/.zshrc-omztemp
+  mv -f ~/.zshrc-omztemp ~/.zshrc
 fi
-
-chsh -s $(which zsh)
 
 # autojump
 sudo apt install autojump -y
@@ -32,4 +43,13 @@ fi
 
 if [ $SHELL == $(which zsh) ] ; then
   source ~/.zshrc
+fi
+
+TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+  if hash chsh >/dev/null 2>&1; then
+    chsh -s $(grep /zsh$ /etc/shells | tail -1)
+  else
+    printf "I can't change your shell automatically because this system does not have chsh.\n"
+  fi
 fi
